b57b45fd383ad014ff7c620c5724ec87246fe03d
ChangZhuo Chen (陳昌倬) <czchen@czchen.org>
2017-06-04 17:33:03 +0800

    Add snap.py

---
+++ lib/ansible/modules/packaging/os/snap.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+# (c) 2017 ChangZhuo Chen (陳昌倬) <czchen@czchen.org>
+#
+# This module is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this software.  If not, see <http://www.gnu.org/licenses/>.
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
